9ca884e7d9337f5bcc6073428bc5eb717fc3e886
rkdms0327 <55428479+rkdms0327@users.noreply.github.com>
2019-10-08 16:18:42 +0900

    07

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+/*	
 //project 1
 	
 	int input;
@@ -56,7 +56,43 @@ int main(int argc, char *argv[]) {
 	for(i=0;i<10;i++)
 	 printf("Hello World!\n");
 	 
-	  
+	 
+//project 6
+	int num=0;
+	char c;
 	
+	printf("input a string : ");
+	
+	while((c=getchar())!='\n')
+	{
+		if (c<='9' && c >='0')
+		{
+			num++;
+		}
+	}
+	
+	printf("%i\n", num);
+*/
+//project 7
+
+	int answer=59;
+	int input;
+	int cnt=0;
+	
+	do
+	{
+		printf("Gess a number :");
+		scanf("%d", &input);
+		
+		if(input>answer)
+		 printf("high!\n");
+		else if (input<answer)
+		 printf("low!\n");
+	
+	 cnt++;
+	}
+	while(input!=answer);
+	
+	printf("congratulation! trial : %i\n", cnt);
 	return 0;
 }
