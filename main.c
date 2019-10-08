3a3e554f9ebaf88f8f99a92abad8cf0525a137d3
rkdms0327 <55428479+rkdms0327@users.noreply.github.com>
2019-10-08 15:30:44 +0900

    05

--- main.c
+++ main.c
@@ -4,7 +4,31 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+//project 1
+	
 	int input;
+	
+	printf("input a number : ");
+	scanf("%d", &input);
+	
+	if(input>0)
+	 printf("positive.\n");
+	else
+	 printf("non-positive.\n");
+	
+//project 2
+
+	printf("input a number : ");
+	scanf("%d", &input);
+	
+	if(input>0)
+	 printf("|x|=%d\n", input);
+	else
+	 printf("|x|=%d\n", -input);
+
+//project 3
+	
 	
 	printf("input a number : ");
 	scanf("%d", &input);
@@ -16,5 +40,23 @@ int main(int argc, char *argv[]) {
 	else
 	 printf("zero.\n");
 	 
+	 
+//project 4
+	
+	int i=0, meter;
+	
+	while(i<3)
+	{
+		meter=i*1609;
+		printf("%d mile is %d meter.\n", i, meter);
+		i++;
+	}
+	 
+//project 5
+	for(i=0;i<10;i++)
+	 printf("Hello World!\n");
+	 
+	  
+	
 	return 0;
 }
